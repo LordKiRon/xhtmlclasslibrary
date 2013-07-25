3fa37b8b4823f5127fc54d0bd2c60ab57e41329a
lordkiron@gmail.com <lordkiron@gmail.com@37577cb7-c9fc-2187-d827-5c6ce6d22976>
2013-07-25 17:07:48 +0000

    Fixed the wrongly categorized "pre" element

---
+++ BaseElements/InlineElements/PreFormated.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using XHTMLClassLibrary.Attributes;
+using XHTMLClassLibrary.BaseElements.BlockElements;
+
+namespace XHTMLClassLibrary.BaseElements.InlineElements
+{
+    public class PreFormated : TextBasedElement
+    {
+        internal const string ElementName = "pre";
+
+        private readonly XMLSpaceAttribute _xmlspaceAttribute = new XMLSpaceAttribute();
+
+
+        /// <summary>
+        /// This attribute indicates if white space (extra spaces, tabs) should be preserved. 
+        /// Possible value is "preserve".
+        /// </summary>
+        public XMLSpaceAttribute PreserveSpace { get { return _xmlspaceAttribute; } }
+
+        protected override string GetElementName()
+        {
+            return ElementName;
+        }
+    }
+}
